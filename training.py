2b0eac3fd092d372d56279d449dbd9293764dcbd
Jack Jewett <tum0rman313@pop-os.localdomain>
2022-06-13 16:54:00 -0400

    Training on variable basics in python. this has also been added to the code training slideshow

---
+++ training.py
@@ -0,0 +1,21 @@
+import wheel
+
+# importing packages gives you access to premade methods to make coding easier.
+
+from random import *
+
+# when importing variables like this you do not need to call the package before the method
+
+myVar = 5
+# declaring variables in python is simple, you don't have to specify variable type, and
+# no semicolons needed.
+
+myVar += 1
+# you can modify variables by using modifiers like +=, -=, *=, /=, etc.
+
+if myVar == 6:
+    print("my variable is 6")
+# if statements check variable states and will run the commands inside of it if the variable check returns true
+
+randVar = randint(1, 10)
+# a variable can be set to a method imported from a package
